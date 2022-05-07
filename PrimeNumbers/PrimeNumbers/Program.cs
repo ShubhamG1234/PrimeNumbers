2b4cfffc9bc0dd5e44cce409e86142fd569fcfec
ShubhamG1234 <100414848+ShubhamG1234@users.noreply.github.com>
2022-05-07 16:54:37 +0530

    Update Program.cs

--- PrimeNumbers/PrimeNumbers/Program.cs
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -3,17 +3,19 @@ a.I / P ­> Year, ensure it is a 4 digit number.
 b. Logic ­> Determine if it is a Leap Year.
 c. O/P ­> Print the year is a Leap Year or not.
     */
+
+
 using System;
 
 
+       Console.WriteLine("Enter the Year");
 
-    
-            Console.WriteLine("Enter the Year");
+int Year = int.Parse(Console.ReadLine());
 
-            int Year = int.Parse(Console.ReadLine());
+
 
 if ((Year % 4 == 0) && (Year % 100 != 0))
-    Console.WriteLine("Leap Year");
+       Console.WriteLine("Leap Year");
 
 else Console.WriteLine("Not Leap Year");
      
